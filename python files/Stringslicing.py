fb8bc6a252c5015b70a2e9394bbf2e73c3e351fb
SafiyyahKazim <safiyyahkazim@gmail.com>
2021-01-08 11:26:13 -0500

    hello

---
+++ python files/Stringslicing.py
@@ -0,0 +1,6 @@
+tvshows = ['star trek', 'game of thrones','stranger things', 'big bang theory', 'black mirror']
+print(tvshows[0:3])
+print(tvshows[2:4])
+print(tvshows[:3])
+print(tvshows[2:])
+print(tvshows[-2:])
