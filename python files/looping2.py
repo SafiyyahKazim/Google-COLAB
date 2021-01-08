27e89f1b99ad4165f5bfe30748a8ce53126f650c
SafiyyahKazim <safiyyahkazim@gmail.com>
2021-01-08 11:44:13 -0500

    january 8

---
+++ python files/looping2.py
@@ -0,0 +1,7 @@
+tvshows = ['star trek', 'game of throne', 'stranger things']#we start by defining a list
+
+for show in tvshows:# define a for loop, this tells pyhton to pull a name from the list tvshows and store the variable in show
+  
+  print(show)#print the tvshows that were just stored in show
+  
+  #repeats items 2 and 3 until all of the tvshows are printed
